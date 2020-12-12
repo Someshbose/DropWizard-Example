e8b7ef1dadba0ccaa03827148f2543de09ed9f20
Someshbose <iamsomeshbose@gmail.com>
2020-12-12 16:03:09 +0530

    Hello world example of DropWizard

---
+++ src/main/java/someshbose/github/io/HelloWorldApplication.java
@@ -0,0 +1,39 @@
+package someshbose.github.io;
+
+import io.dropwizard.Application;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import someshbose.github.io.app.HelloWorldConfiguration;
+import someshbose.github.io.app.HelloWorldResource;
+import someshbose.github.io.app.TemplateHealthCheck;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+  public static void main(String[] args) throws Exception {
+    new HelloWorldApplication().run("server", "example.yml");
+  }
+
+  @Override
+  public String getName() {
+    return "Hello-world";
+  }
+
+  @Override
+  public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+    bootstrap.setConfigurationSourceProvider(new ResourceConfigurationSourceProvider());
+    super.initialize(bootstrap);
+  }
+
+  @Override
+  public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+    final HelloWorldResource resource =
+        new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
+    
+    final TemplateHealthCheck healthCheck=new TemplateHealthCheck(configuration.getTemplate());
+    
+    environment.healthChecks().register("template", healthCheck);
+    environment.jersey().register(resource);
+  }
+
+}
